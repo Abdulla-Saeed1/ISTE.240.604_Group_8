4bc5403d92393f99915eeaf18f81d3c144589037
hamad bin dasmal <hdb2401@g.rit.edu>
2026-02-16 22:37:18 +0400

    Add Booking model class

---
+++ Project/src/main/java/edu/rit/project/model/Booking.java
@@ -0,0 +1,52 @@
+package edu.rit.project.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Booking {
+    private int bookingId;
+    private int userId;
+    private int sessionId;
+    private String bookingDate;
+    private String status;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(String bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
