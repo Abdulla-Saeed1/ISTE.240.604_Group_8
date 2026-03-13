081c41a2a484204b944e2600eef2359beaaaa330
Abdulla_Almarri <asa4195@rit.edu>
2026-03-13 16:08:11 +0400

    Added success page and updated FitReserveController to handle success route

--- Project/src/main/java/edu/rit/project/FitReserveController.java
+++ Project/src/main/java/edu/rit/project/FitReserveController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,5 +36,11 @@ public class FitReserveController {
                           @RequestParam String membershipType) {
         service.addUser(new User(userId, name, email, phoneNumber, password, membershipType));
         return "redirect:/add/success/user";
+    }
+
+    @GetMapping("/add/success/{entity}")
+    public String showSuccessPage(@PathVariable String entity, Model model) {
+        model.addAttribute("entity",entity);
+        return "success";
     }
 }
