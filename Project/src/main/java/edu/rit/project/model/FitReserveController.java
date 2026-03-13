3ab1363ebfd1cc86dc07ca171d307d863ad4e2c5
Abdulla_Almarri <asa4195@rit.edu>
2026-03-13 14:28:17 +0400

    Created FitReserveController and added user routes

---
+++ Project/src/main/java/edu/rit/project/model/FitReserveController.java
@@ -0,0 +1,39 @@
+package edu.rit.project.model;
+
+import edu.rit.project.FitReserveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FitReserveController {
+
+    @Autowired
+    private FitReserveService service;
+
+
+    @GetMapping("/users")
+    public String showUsers(Model model){
+        model.addAttribute("users", service.getUsers());
+        return "users";
+    }
+
+    @GetMapping("/users/add")
+    public String addUserPage() {
+        return "add-user";
+    }
+
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam int userId,
+                          @RequestParam String name,
+                          @RequestParam String email,
+                          @RequestParam int phoneNumber,
+                          @RequestParam String password,
+                          @RequestParam String membershipType) {
+        service.addUser(new User(userId, name, email, phoneNumber, password, membershipType));
+        return "redirect:/add/success/user";
+    }
+}
