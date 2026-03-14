8d9aa94584e30ec63739f53786db2fddec35661b
Khalifa_Alhammadi <kaa9693@rit.edu>
2026-03-14 06:11:28 -0700

    Added Trainer methods to FitReserveService

--- Project/src/main/java/edu/rit/project/FitReserveService.java
+++ Project/src/main/java/edu/rit/project/FitReserveService.java
@@ -2,6 +2,7 @@ package edu.rit.project;
 
 import edu.rit.project.model.Booking;
 import edu.rit.project.model.FitnessSession;
+import edu.rit.project.model.Trainer;
 import edu.rit.project.model.User;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
@@ -10,12 +11,16 @@ import java.util.List;
 @Service
 public class FitReserveService {
     private List<User> users = new ArrayList<>();
+    private List<Trainer> trainers = new ArrayList<>();
     private List<FitnessSession> sessions = new ArrayList<>();
     private List<Booking> bookings = new ArrayList<>();
 
     public FitReserveService() {
         users.add(new User(101, "Ahmad", "Ahmad@Gmail.com",0551234567, "Ahmad-131", "Standard"));
         users.add(new User(102,"Khalid","Khalid@Gmail.com",0551225432,"Khal1d-21","Premium" ));
+
+        trainers.add(new Trainer("Omar Ali", 201,"Strength Training",6,"Sunday-Thursday",4.6));
+        trainers.add(new Trainer("Sara Hassan", 202,"Yoga",5,"Monday-Friday",4.8));
 
         bookings.add(new Booking(401, 101, 301, "2026-03-05", "Confirmed"));
         bookings.add(new Booking(402, 102, 302, "2026-03-06", "Pending"));
@@ -28,6 +33,14 @@ public class FitReserveService {
 
     public void addUser(User user) {
         users.add(user);
+    }
+
+    public List<Trainer> getTrainers() {
+        return trainers;
+    }
+
+    public void addTrainer(Trainer trainer) {
+        trainers.add(trainer);
     }
 
     public List<Booking> getBookings() {
