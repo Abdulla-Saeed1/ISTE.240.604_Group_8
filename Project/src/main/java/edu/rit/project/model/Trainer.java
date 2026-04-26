64c8d50723082616079231fffe2db1b06f9bbd63
Khalifa_Alhammadi <kaa9693@rit.edu>
2026-04-26 12:55:41 -0700

    Update Trainer entity

--- Project/src/main/java/edu/rit/project/model/Trainer.java
+++ Project/src/main/java/edu/rit/project/model/Trainer.java
@@ -1,6 +1,19 @@
 package edu.rit.project.model;
 
+/*
+ * Author: Khalifa Alhammadi (ID: 399001487)
+ * Model class representing a trainer in the FitReserve system.
+ * Stores trainer details such as specialization, experience, availability, and rating.
+ */
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "trainers")
 public class Trainer {
+    @Id
     private int trainerId;
     private String name;
     private String specialization;
@@ -8,60 +21,32 @@ public class Trainer {
     private String availabilitySchedule;
     private double rating;
 
+    public Trainer() {}
+
     public Trainer(String name, int trainerId, String specialization, int experienceYears, String availabilitySchedule, double rating) {
         this.name = name;
-        this.trainerId = trainerId; 
-        this.specialization = specialization;
-        this.experienceYears = experienceYears;
-        this.availabilitySchedule = availabilitySchedule;
-        this.rating = rating;
-    }
-
-    public int getTrainerId() {
-        return trainerId;
-    }
-
-    public void setTrainerId(int trainerId) {
         this.trainerId = trainerId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(String specialization) {
         this.specialization = specialization;
-    }
-
-    public int getExperienceYears() {
-        return experienceYears;
-    }
-
-    public void setExperienceYears(int experienceYears) {
         this.experienceYears = experienceYears;
-    }
-
-    public String getAvailabilitySchedule() {
-        return availabilitySchedule;
-    }
-
-    public void setAvailabilitySchedule(String availabilitySchedule) {
         this.availabilitySchedule = availabilitySchedule;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
         this.rating = rating;
     }
+
+    public int getTrainerId() { return trainerId; }
+    public void setTrainerId(int trainerId) { this.trainerId = trainerId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+
+    public int getExperienceYears() { return experienceYears; }
+    public void setExperienceYears(int experienceYears) { this.experienceYears = experienceYears; }
+
+    public String getAvailabilitySchedule() { return availabilitySchedule; }
+    public void setAvailabilitySchedule(String availabilitySchedule) { this.availabilitySchedule = availabilitySchedule; }
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
 }
