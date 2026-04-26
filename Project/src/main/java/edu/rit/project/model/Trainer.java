a71e9f82f9e84cafb493215a25a51d09b2f73b4a
Khalifa_Alhammadi <kaa9693@rit.edu>
2026-04-26 13:02:30 -0700

    Add Trainer repository with JPA methods

--- Project/src/main/java/edu/rit/project/model/Trainer.java
+++ Project/src/main/java/edu/rit/project/model/Trainer.java
@@ -1,11 +1,19 @@
 package edu.rit.project.model;
 
 /*
+ * Author: Khalifa Alhammadi (ID: 399001487)
  * Model class representing a trainer in the FitReserve system.
- * This class stores trainer details such as specialization, experience, availability, and rating.
+ * Stores trainer details such as specialization, experience, availability, and rating.
  */
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "trainers")
 public class Trainer {
+    @Id
     private int trainerId;
     private String name;
     private String specialization;
@@ -13,7 +21,8 @@ public class Trainer {
     private String availabilitySchedule;
     private double rating;
 
-    // Constructor used to create a new trainer object
+    public Trainer() {}
+
     public Trainer(String name, int trainerId, String specialization, int experienceYears, String availabilitySchedule, double rating) {
         this.name = name;
         this.trainerId = trainerId;
@@ -23,52 +32,21 @@ public class Trainer {
         this.rating = rating;
     }
 
-    // Getter and setter methods for trainer information
-    public int getTrainerId() {
-        return trainerId;
-    }
+    public int getTrainerId() { return trainerId; }
+    public void setTrainerId(int trainerId) { this.trainerId = trainerId; }
 
-    public void setTrainerId(int trainerId) {
-        this.trainerId = trainerId;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName() {
-        return name;
-    }
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public int getExperienceYears() { return experienceYears; }
+    public void setExperienceYears(int experienceYears) { this.experienceYears = experienceYears; }
 
-    public String getSpecialization() {
-        return specialization;
-    }
+    public String getAvailabilitySchedule() { return availabilitySchedule; }
+    public void setAvailabilitySchedule(String availabilitySchedule) { this.availabilitySchedule = availabilitySchedule; }
 
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
-
-    public int getExperienceYears() {
-        return experienceYears;
-    }
-
-    public void setExperienceYears(int experienceYears) {
-        this.experienceYears = experienceYears;
-    }
-
-    public String getAvailabilitySchedule() {
-        return availabilitySchedule;
-    }
-
-    public void setAvailabilitySchedule(String availabilitySchedule) {
-        this.availabilitySchedule = availabilitySchedule;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
 }
